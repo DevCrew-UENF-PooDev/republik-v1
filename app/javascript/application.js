9934479f4402d947f9db98dd436284fdfc39a932
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2024-11-10 19:08:46 -0300

    style: navbar/sidebarr
    Co-authored-by: Estefânio Silva Ribeiro <Estef4nio@users.noreply.github.com>

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,28 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+function isAuthenticated() 
+{
+    const path = window.location.pathname;
+    return path !== "/usuarios/sign_in" && path !== "/usuarios/sign_up"
+}
+
+function toggleDropdown() 
+{
+    if(!isAuthenticated()) return;
+
+    const avatarButton = document.getElementById("avatar-button");
+    const dropdownMenu = document.getElementById("dropdown-menu");
+    avatarButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Evita qualquer ação padrão
+        dropdownMenu.classList.toggle("hidden"); // Alterna a visibilidade do menu
+    });
+    window.addEventListener("click", function(event) {
+        if (!avatarButton.contains(event.target) && !dropdownMenu.contains(event.target)) {
+            dropdownMenu.classList.add("hidden");
+        }
+    });
+}
+
+document.addEventListener('turbo:load', toggleDropdown);
