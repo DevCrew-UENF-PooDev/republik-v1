1c72915add74140318917ee86dd6c12cc85f79c3
Marcos Dias <57465032+SoraAsc@users.noreply.github.com>
2024-11-13 21:57:24 -0300

    style: comment and post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     # Carregar todas as comunidades
     @comunidades = Comunidade.all
-    # Também carregar as comunidades em que o usuário participa (opcional, para exibição diferente)
+    # Também carregar as comunidades em que o usuário participa
     @comunidades_participadas = current_usuario.comunidades.pluck(:id)
     @postagens = Postagem.all
   end
